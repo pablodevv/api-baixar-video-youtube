afd2b712ee1559759d57e3df3c566a10d2ec0852
Pablo Eduardo <pagamentos.pablorodriguez@gmail.com>
2025-03-07 17:11:12 -0300

    atualização2

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 const app = express();
 app.use(cors());
 
-
 app.get("/", (req, res) => {
     const ping = new Date();
     ping.setHours(ping.getHours() - 3);
@@ -39,22 +38,26 @@ app.get("/mp3", async (req, res) => {
     if (url) {
         const tempFilePath = path.join(__dirname, 'tmp', 'download.mp3');
 
+        // Comando para baixar o áudio e convertê-lo para MP3 usando yt-dlp
         exec(`yt-dlp -x --audio-format mp3 --output "${tempFilePath}" ${url}`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`Error executing yt-dlp: ${stderr}`);
                 return res.status(500).send('Failed to download audio');
             }
 
+            // Extrai o nome do vídeo sem a extensão
             const videoName = path.basename(tempFilePath, '.mp3');
             res.header("Content-Disposition", `attachment; filename="${videoName}.mp3"`);
             res.header("Content-type", "audio/mpeg");
 
+            // Verifica se o arquivo existe antes de enviar
             if (fs.existsSync(tempFilePath)) {
                 res.sendFile(tempFilePath, (err) => {
                     if (err) {
                         console.error("Error sending file: ", err);
                         res.status(500).send('Failed to send file');
                     } else {
+                        // Deleta o arquivo após o envio
                         fs.unlink(tempFilePath, (unlinkErr) => {
                             if (unlinkErr) {
                                 console.error("Error deleting file: ", unlinkErr);
