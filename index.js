3122e8e9bfef88b91f1ea7e1f4909213740acebf
Pablo Eduardo <pagamentos.pablorodriguez@gmail.com>
2025-03-10 13:48:28 -0300

    nova 2

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
 const cors = require("cors");
 const { exec } = require("child_process");
 
 const corsOptions = {
   origin: "https://api-baixar-video-youtube.onrender.com",
-=======
-const ytdl = require("ytdl-core");
-const cors = require("cors");
-
-const corsOptions = {
-  origin: "https://api-baixar-video-youtube.onrender.com", 
->>>>>>> 67dfbb3c2ccddd1085dc1a9bf0c785b32fecf5fc
   credentials: true,
   optionSuccessStatus: 200,
   exposedHeaders: "**",
@@ -24,7 +16,6 @@ app.get("/baixar", async (req, res, next) => {
   console.log(req.query.url);
   try {
     const videoUrl = req.query.url;
-<<<<<<< HEAD
 
     // Comando do yt-dlp para pegar o melhor áudio
     const command = `yt-dlp -f bestaudio --extract-audio --audio-format mp3 --quiet --no-warnings ${videoUrl}`;
@@ -43,13 +34,6 @@ app.get("/baixar", async (req, res, next) => {
       // A resposta pode ser o link do áudio ou algo gerado após o download
       console.log(`stdout: ${stdout}`);
       res.send(stdout); // Ajuste conforme necessário (por exemplo, retornando o URL do áudio ou o caminho do arquivo)
-=======
-    const videoInfo = await ytdl.getInfo(videoUrl);
-    const audioFormats = ytdl.filterFormats(videoInfo.formats, "audioonly");
-    console.log(audioFormats);
-    audioFormats.map((item) => {
-      res.send(item.url);
->>>>>>> 67dfbb3c2ccddd1085dc1a9bf0c785b32fecf5fc
     });
   } catch (error) {
     next(error);
@@ -57,10 +41,5 @@ app.get("/baixar", async (req, res, next) => {
 });
 
 app.listen(process.env.PORT || 8100, () => {
-<<<<<<< HEAD
   console.log("Server on");
 });
-=======
-    console.log("Server on");
-});
->>>>>>> 67dfbb3c2ccddd1085dc1a9bf0c785b32fecf5fc
