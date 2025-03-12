428f983c567cbdee5a7803ed956a10c9d7853114
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-03-12 17:46:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ async function convertWithRetry(page, videoUrl, maxRetries = 3, retryDelay = 100
     let retries = 0;
     while (retries < maxRetries) {
         try {
+            console.log(`Tentativa ${retries + 1}: Enviando URL do vídeo`);
             await page.type('input[placeholder="Enter Youtube URL"]', videoUrl);
             await page.click('button[class*="bg-[#4F46E5]"]');
-            await page.waitForSelector('audio source', { timeout: 300000 });
+            console.log('Clicando no botão de conversão...');
+            await page.waitForSelector('audio source', { timeout: 600000 }); // Aumentando o tempo de espera para 10 minutos
+            console.log('Link de áudio encontrado!');
             return; // Conversão bem-sucedida
         } catch (error) {
             console.error(`Erro na conversão (tentativa ${retries + 1}):`, error);
             retries++;
             if (retries < maxRetries) {
+                console.log('Tentando novamente após 10 segundos...');
                 await new Promise(resolve => setTimeout(resolve, retryDelay));
                 await page.reload(); // Recarrega a página antes de tentar novamente
             } else {
@@ -39,12 +43,14 @@ app.get('/download', async (req, res) => {
         const browser = await puppeteer.launch({
             headless: 'new',
             args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-cache'],
-            protocolTimeout: 300000, // Aumenta o protocolTimeout
+            protocolTimeout: 600000, // Aumenta o protocolTimeout para 10 minutos
         });
         const page = await browser.newPage();
 
+        console.log('Acessando a página de conversão...');
         await page.goto('https://app.aiseo.ai/tools/youtube-to-mp3', { timeout: 60000 });
 
+        console.log('Aguardando carregamento do seletor...');
         await page.waitForSelector('input[placeholder="Enter Youtube URL"]');
 
         await convertWithRetry(page, videoUrl);
@@ -57,6 +63,7 @@ app.get('/download', async (req, res) => {
         await browser.close();
 
         if (downloadLink) {
+            console.log('Link de download encontrado:', downloadLink);
             res.json({ link: downloadLink });
         } else {
             res.status(500).json({ error: 'Link de download não encontrado.' });
