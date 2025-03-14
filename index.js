1ec792f7f4c749d41e45f45757bc3e78aa5ed24c
pablodevv <79975765+pablodevv@users.noreply.github.com>
2025-03-14 16:17:11 -0300

    Update index.js json

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const puppeteer = require('puppeteer');
-const dropboxV2Api = require('dropbox-v2-api');
 const cors = require('cors');
-
-// Configuração do Dropbox API
-const dropbox = dropboxV2Api.authenticate({
-  token: 'sl.u.AFnN5jjWuoNG3WYngPTaTlHE0cjkS1wIIuZzeUZPtkzT1qa6pCYgKIoTxqKLwzWl_XJYQD04ed26cyDTvCqQ8uqlFVUXbrcDCc1Zr1J0mrgBf3fkrL4mSvZGGPZZ6y7QU6MG1ALfkWq57aQ1yZO3AhSEGba4fqZGyhCoGmWSLQGmBpvOvPlRK5ZLUD2c8ti0SA1hPy_vJ0VRBaVYXENqm8-DnIkx9oniX2dMsy_wcmnoVH_ig6hrVznoMBPAQcHNmd7P-dwfPYdHVExWfeKY1lzWCRzLKxPpOFNCbS6zsRmcvKM84HH5w5XG02ERWSAD_2ZRhF850O-BQk4lPHVCw6UsnRNF-Unl3c1Tsy9jJL4R57zt7h7iCbelfTrkQw1JhFsgxlF6BuwP6nT7Ehb4u6IH_r_COg3bCNR7FZX4rLuxi0M9fMJt1heoUGWwrNlUW5EYpDSDNW6SvmfD_dt3KTmSEZZAPMXJed2AVlARrsY3irkJVbZ3Z7yNNHukUSXZFKW28VUcIFfZdmJN5guPDCng9wr2HWLFMVV6HjfSsWu-A9wMR-rHOZUvpvv1UO9YoQzYVNOzgHETM90n1WyBi2IbfdngxRgc-nZDqVAr77stubn8vzHXu0PTyVgbPE8uZ5YlPk-IZmOvKBSZfejDsDjM7H6tJQtu0XOPNsSb0KEdkDRUg41zD4PZgsCpGCDsN3jHHTS1udFblyHtv-JS9BNZiPomXIbyfK4Y_p6QcQInjjcYgzgkBk9eTR3uQYZCjHOEjfqZxPy2bG_zyJdc-d6mlLYGYWXBXzWALQqEKfEzachUewsWFR-oFHeUZaamTKh4nALxUAMvl6e38Q2k8FhV2Mw_96qaorzo-tmX6mE84TDg8t_Tr0nvgnHZ-61PrpbalWfu137IX4OcScNaWb5monnOC2bBmlen2gEWif8uu8OWwIvadnHs3u0EEcU56wVkYbgG55DnqOD2cOSOMbOK4305-_FU-_POl02x-5RlxcZKU5ErfQI_EdSSX8jKKtrWYCWSKo5DVFhrgaN0tdr2oMY2Ry8vI2pSI1UnYTUBBOO6-P45dM-6awA7Fi8vEjtjRpgUUGIgtvpnMo6oRDMm7bo2-SFI2ER9dfUqKbBPPCLAqT_u3Ajq6PDf8411PoW1f3130-PPvKhjDLiQLrBDSHcugtnYVAqQp16vxcAx9RCzI3iXfNvE6r12qr0PBz7kQT9kZAQ7KOArpQFo3phqf5kCiqlkPMKhP_r3I1g4kq66BHiPAnnp81vHz_6hG8eUqIcnrMNr9vhA3lx5j6QxA4Z9F3DNQexPZ7PvzMa94Sfte7yakcTU-A_9LV3DY7x5O-w8XRv_XUUnzK6I6bnm_Tx7zerg8gYX13ONPOEouhndsRR_GqZamq-MjRNGn6mgquRFs86-4QIJF-37i9sLxjDJlr0qH4AS2gCZg7EAFDIerRdEi3IVOxlh4Ud1m_Q6DqM1lkNlo2pY4C1x8eig'
-});
 
 const app = express();
 const port = 8100;
@@ -15,7 +9,7 @@ const port = 8100;
 // Middleware CORS
 app.use(cors());
 
-// Rota para baixar o MP3 e fazer o upload no Dropbox
+// Rota para converter o vídeo em áudio e retornar o link do áudio
 app.get('/download', async (req, res) => {
   const videoUrl = req.query.url;  // URL do vídeo que será convertido para MP3
   const fileName = req.query.name;  // Nome do arquivo MP3 (título do vídeo)
@@ -50,38 +44,14 @@ app.get('/download', async (req, res) => {
     console.log(`Título do vídeo: ${fileName}`);
     console.log(`URL do áudio: ${audioSrc}`);
 
-    // Fazendo o download do arquivo MP3 com headers para evitar erro 403
-    const response = await axios.get(audioSrc, {
-      responseType: 'arraybuffer',
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Connection': 'keep-alive',
-        'Accept-Encoding': 'gzip, deflate, br'
-      }
+    // Retornar o link de áudio em formato JSON
+    res.json({
+      success: true,
+      message: 'Áudio convertido com sucesso!',
+      audio_url: audioSrc
     });
 
-    // Upload para o Dropbox diretamente com o buffer de dados
-    const dropboxPath = `/Vídeos YT VR System/${fileName}.mp3`;
-
-    console.log(`Fazendo upload para o Dropbox...`);
-
-    dropbox.filesUpload({
-      path: dropboxPath,
-      contents: response.data,
-      autorename: true  // Caso o arquivo já exista, o Dropbox irá renomeá-lo automaticamente
-    })
-    .then(() => {
-      console.log(`Arquivo ${fileName}.mp3 enviado com sucesso para o Dropbox!`);
-      res.send(`Arquivo ${fileName}.mp3 enviado com sucesso para o Dropbox!`);
-    })
-    .catch((error) => {
-      console.error('Erro no upload para o Dropbox:', error);
-      res.status(500).send('Erro ao enviar para o Dropbox.');
-    })
-    .finally(() => {
-      browser.close();  // Fecha o navegador
-    });
+    browser.close();  // Fecha o navegador
 
   } catch (error) {
     console.error('Erro ao processar o vídeo:', error);
